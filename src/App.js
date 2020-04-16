05037fc3e770d060a738e99e05a8561aa7deec92
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-15 20:30:56 -0400

    Created a components folder - Added a working Header component with the Title of the web page -  Also created the Card component (not working yet)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Card from "./components/Card";
+
+
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun 🚀!
       </p>
+      <Header/>
+      
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div>
+            <h1>Nasa Photo of the Day</h1>
+        </div>
+    );
+}
+
+export default Header;
