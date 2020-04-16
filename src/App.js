eb98fe7529f579c211c81a4d84987a0651e4646a
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-15 23:25:26 -0400

    Added the Card.js component (working). Added styles to the Card via App.css

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import Card from "./components/Card";
 
 
 function App() {
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      
       <Header/>
+      <Card />
       
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Header(){
     return(
-        <div>
-            <h1>Nasa Photo of the Day</h1>
+        <div>            
+            <h1>Nasa Photo of the Day 🚀</h1>
         </div>
     );
 }
